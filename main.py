b43c9524c697c50de0bd3a526cc52cdb90b92e52
BittahD <theamazingdesperado@gmail.com>
2023-11-06 14:01:50 -0600

    changed main.py code

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-with open("books/frankenstein.txt") as book:
-    print(book.read())
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
+    
+main()
