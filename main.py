e592409bed57b68dd4e8235fc9b47042ea600c2b
BittahD <theamazingdesperado@gmail.com>
2023-11-07 00:40:06 -0600

    added a letter count function that adds key->value pairs to a dict, and increases count for each letter

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import re
+
 def main():
+    #path to the book
     book_path = "books/frankenstein.txt"
+    #read .txt file into CLI
     text = get_book_text(book_path)
+    #word count within a .txt file        
     words = get_word_count(text)
-    print(f"This book has {words} words and ")
+    #dict with individual letter count
+    letters = {}
+    get_letter_count(text, letters)
+    print(f"This book has {words} words and {letters} ")
     
 def get_book_text(path):
     with open(path) as book:
@@ -11,4 +19,14 @@ def get_book_text(path):
 def get_word_count(book):
     words = book.split()
     return len(words)
+
+def get_letter_count(text, letter_dict):
+    for index, letter in enumerate(text):
+        if letter not re.match([A-Za-z]):
+            continue
+        if letter not in letter_dict:
+           letter_dict.setdefault(letter.lower(), 1)
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+
 main()
